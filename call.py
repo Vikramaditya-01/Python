533c9eab50a2d7c9b7b9ebb21370e384869fa3dc
Vikramaditya <vikramadityagorai@gmail.com>
2025-03-23 02:57:19 +0530

    Day one of Python

---
+++ call.py
@@ -0,0 +1,3 @@
+# from module_name import function_name
+from hello import hello
+hello("Hello from vikramaditya")
---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+def hello(n):
+    print(n)
+
+hello("Hello from vikramaditya")    
+
+name_one = "Munmun"
+name_two = "Vikramaditya"
+name_three = "Munmun Gorai"
+name_four = "Vikramaditya Gorai"
