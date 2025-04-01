3f915751464b2cef6bd7c8a5ed588328ce483eb1
Vikramaditya <vikramadityagorai@gmail.com>
2025-04-01 23:45:19 +0530

    Docerators

---
+++ 07_Decorators/Timing_function.py
@@ -0,0 +1,17 @@
+import time 
+
+def timer(function):
+    def wrapper(*args , **kwargs):
+        start = time.time()
+        result = function(*args, **kwargs)
+        end = time.time()
+        print(f"{function.__name__} ran in {end - start}")
+        return result
+    return wrapper
+
+
+@timer
+def example_function(n):
+    time.sleep(n)
+
+example_function(2)
